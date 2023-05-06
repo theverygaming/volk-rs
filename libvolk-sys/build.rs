3f652eb2284d5e84d28305d85a0dbe4781e3465f
theverygaming <clemibunge@gmail.com>
2023-05-06 19:12:15 +0200

    github actions

--- libvolk-sys/build.rs
+++ libvolk-sys/build.rs
@@ -1,6 +1,14 @@
 fn main() {
     if cfg!(target_os = "windows") {
-        println!("cargo:rustc-link-lib=libvolk");
+        println!("cargo:rustc-link-search={}", "./");
+        println!(
+            "cargo:rustc-link-search={}",
+            std::path::PathBuf::from(
+                std::env::var("VOLK_LIB_DIR").expect("VOLK_LIB_DIR must be set to a valid path")
+            )
+            .display()
+        );
+        println!("cargo:rustc-link-lib=volk");
     } else {
         println!("cargo:rustc-link-lib=volk");
     }
