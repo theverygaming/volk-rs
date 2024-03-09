86ed8fa86ca0f21ab8daf2414a5777a3bbb25c0b
theverygaming <theverygaming@users.noreply.github.com>
2024-03-09 10:24:32 +0100

    windows CI..

--- libvolk-sys/build.rs
+++ libvolk-sys/build.rs
@@ -1,8 +1,6 @@
 fn main() {
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     if cfg!(target_os = "windows") {
-        println!("cargo:rustc-link-search={}", out_dir.display());
-        println!("cargo:rustc-link-lib=volk");
         let volkdir = std::path::PathBuf::from(std::env::var("VOLK_LIB_DIR").expect("VOLK_LIB_DIR must be set to a valid path"));
 
         if !out_dir.join("volk.dll").exists() {
@@ -11,6 +9,8 @@ fn main() {
         if !out_dir.join("volk.lib").exists() {
             std::fs::copy(volkdir.join("volk.lib"), out_dir.join("volk.lib")).unwrap();
         }
+        println!("cargo:rustc-link-lib=cdylib=volk");
+        println!("cargo:rustc-link-search=native={}", out_dir.display());
     } else {
         println!("cargo:rustc-link-lib=volk");
     }
